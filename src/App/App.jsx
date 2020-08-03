fbd39425b54ee556caed1027d5523ecb905a7e2a
Samir Rashid <samirsgotmail@gmail.com>
2020-08-02 17:11:15 -0700

    More project structure and initial NavBar

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import Test from '../Pages/Test/Test';
-import LevelHub from '../Pages/LevelHub/LevelHub';
+import LevelHub from '../Pages/LevelHub/levelHub';
+import footer from '../Pages/UniversalComponents/footer';
 
 import configureStore from './store/configureStore';
 import createMiddleware from './store/createMiddleware';
@@ -17,19 +13,24 @@ import createMiddleware from './store/createMiddleware';
 const { store, persistor } = configureStore(createMiddleware);
 
 const App = () => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/fff" component={() => <div>fff</div>} />
-          <Route path="/test" component={Test} />
-          <Route path="/levelhub" component={LevelHub} />
-          <Route exact path="/" component={() => <div>Hello World!</div>} />
-          <Redirect to="/" />
-        </Switch>
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
+  <div>
+    {/* <header /> */}
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/fff" component={() => <div>fff</div>} />
+            <Route path="/test" component={Test} />
+            <Route path="/levelhub" component={LevelHub} />
+            <Route path="/footer" component={footer} />
+            <Route exact path="/" component={() => <div>Hello World!</div>} />
+            <Redirect to="/" />
+          </Switch>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+    {/* <footer /> */}
+  </div>
 );
 
 export default App;
--- src/Pages/LevelHub/levelHub.jsx
+++ src/Pages/LevelHub/levelHub.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
-import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider,
+} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Checkbox from '@material-ui/core/Checkbox';
+import NavBar from '../UniversalComponents/NavBar';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    color: '#00FF00',
     backgroundColor: theme.palette.primary.light,
   },
   paper: {
     height: 140,
     width: 100,
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   },
   control: {
     padding: theme.spacing(2),
+  },
+  icon: {
+    backgroundColor:
+      'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+    color: 'primary',
+  },
+  checkedIcon: {
+    backgroundColor:
+      'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+    color: 'primary',
   },
 }));
 
@@ -36,11 +51,13 @@ const theme = createMuiTheme({
     },
   },
 });
+
 export default function LevelHub() {
   const classes = useStyles(theme);
 
   return (
     <ThemeProvider theme={theme}>
+      <NavBar />
       <Grid
         container
         classes={{
@@ -57,14 +74,20 @@ export default function LevelHub() {
                     root: classes.paper,
                   }}
                 >
-                  hello this is text
+                  hello this is
+                  {value}
                   <Checkbox
-                    classes={{
-                      root: classes.root,
-                    }}
+                    className={classes.icon}
+                    checkedIcon={<span className={classes.checkedIcon} />}
+
+                    // checkedIcon={
+                    //   <span
+                    //     className={clsx(classes.icon, classes.checkedIcon)}
+                    //   />
+                    // }
+                    // icon={<span className={classes.icon} />}
                   />
                 </Paper>
-
               </Grid>
             ))}
           </Grid>
