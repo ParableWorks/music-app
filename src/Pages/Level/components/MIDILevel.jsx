c846fb3ef20b8ed3aa11f10e34d064aa13caa8d6
Prem Netsuwan <grnnja@gmail.com>
2020-08-12 11:43:31 -0700

    Move VexFlow demo into Level as Sheetmusic and reorganize
    Get rid of Page1 folder since it is not needed anymore for structure demo

--- src/Pages/Level/components/MIDILevel.jsx
+++ src/Pages/Level/components/MIDILevel.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import OnScreenKeyboard from './OnScreenKeyboard';
+import SheetMusic from './SheetMusic';
 
 const MIDILevel = () => {
   return (
     <div>
       this is a midi level
+      <SheetMusic />
       <OnScreenKeyboard />
     </div>
   );
