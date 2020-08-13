5e0bda6f7b92c3164fd9ef731b98e2225858427a
Prem Netsuwan <grnnja@gmail.com>
2020-08-12 22:25:58 -0700

    Allow notes to be changed on the fly
    super jank, also added eslint config for react
    hooks since they seem very complicated

--- src/Pages/Level/components/MIDILevel.jsx
+++ src/Pages/Level/components/MIDILevel.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import OnScreenKeyboard from './OnScreenKeyboard';
 import SheetMusic from './SheetMusic';
 
 const MIDILevel = () => {
+  const [redNotes, setRedNotes] = useState([]);
+  const [greenNotes, setGreenNotes] = useState(["c/4", "f/5"]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (redNotes.length === 0) {
+        setRedNotes(["c/4", "c/5"]);
+      } else {
+        setRedNotes([]);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [redNotes]);
   return (
     <div>
       this is a midi level
-      <SheetMusic notes={["c/4", "e/4", "g/4"]} redNotes={["c/4", "c/5"]} greenNotes={["c/4", "f/5"]} />
+      <br />
+      {redNotes.toString()}
+      <br />
+      <SheetMusic notes={["c/4", "e/4", "g/4"]} redNotes={redNotes} greenNotes={greenNotes} />
       <OnScreenKeyboard />
     </div>
   );
--- src/Pages/Level/components/SheetMusic.jsx
+++ src/Pages/Level/components/SheetMusic.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import Vex from 'vexflow';
 
 const SheetMusic = (props) => {
   const { notes, clef, redNotes, greenNotes } = props;
   const elemRef = useRef();
+
+  const [ renderer, setRenderer ] = useState(false);
+   const VF = Vex.Flow;
 
   /**
    * applies styles to a vex note with multiple keys
@@ -22,59 +25,65 @@ const SheetMusic = (props) => {
     return note;
   };
 
-  const redNoteStyle = {
-    fillStyle: "red",
-    strokeStyle: "red",
-  };
-
-  const greenNoteStyle = {
-    fillStyle: "green",
-    strokeStyle: "green",
-  };
-
   useEffect(() => {
-    const VF = Vex.Flow;
-
-    // Create an SVG renderer and attach it to the DIV element
-    const renderer = new VF.Renderer(elemRef.current, VF.Renderer.Backends.SVG);
-
-    // Configure the rendering context.
-    renderer.resize(800, 100);
-    const context = renderer.getContext();
-    context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
-
-    // Create a stave of width 400 at position 10, 40 on the canvas.
-    const stave = new VF.Stave(0, 0, 800);
-
-    // Add a clef and time signature.
-    // stave.addClef("treble").addTimeSignature("4/4");
-    stave.addClef(clef);
-    stave.setTimeSignature("4/4");
-
-    // Connect it to the rendering context and draw!
-    stave.setContext(context).draw();
-
-    let staveNotes = [
-      // union of notes and redNotes with no duplicates
-      new VF.StaveNote({ clef, keys: [...new Set([...notes, ...redNotes, ...greenNotes])], duration: "w" }),
-    ];
-
-    staveNotes[0] = styleKeys(staveNotes[0], redNotes, redNoteStyle);
-    staveNotes[0] = styleKeys(staveNotes[0], greenNotes, greenNoteStyle);
-
-    // Create a voice in 4/4 and add the notes from above
-    const voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
-    voice.addTickables(staveNotes);
-    // Format and justify the notes to 400 pixels.
-    const formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
-
-    // Render voice
-    voice.draw(context, stave);
-
-    // const beams = VF.Beam.generateBeams(notes);
-    Vex.Flow.Formatter.FormatAndDraw(context, stave, staveNotes);
-    // beams.forEach((b) => { b.setContext(context).draw(); });
+    setRenderer(new VF.Renderer(elemRef.current, VF.Renderer.Backends.SVG));
   }, []);
+
+  // this code seems very very sketchy
+  useEffect(() => {
+    if (renderer) {
+      const redNoteStyle = {
+        fillStyle: "red",
+        strokeStyle: "red",
+      };
+
+      const greenNoteStyle = {
+        fillStyle: "green",
+        strokeStyle: "green",
+      };
+      renderer.resize(800, 100);
+      const context = renderer.getContext();
+      context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+      const group = context.openGroup();
+
+      // Create a stave of width 400 at position 10, 40 on the canvas.
+      const stave = new VF.Stave(0, 0, 800);
+
+      // Add a clef and time signature.
+      // stave.addClef("treble").addTimeSignature("4/4");
+      stave.addClef(clef);
+      stave.setTimeSignature("4/4");
+
+      // Connect it to the rendering context and draw!
+      stave.setContext(context).draw();
+
+      const staveNotes = [
+        // union of notes and redNotes with no duplicates
+        new VF.StaveNote({ clef, keys: [...new Set([...notes, ...redNotes, ...greenNotes])], duration: "w" }),
+      ];
+
+      staveNotes[0] = styleKeys(staveNotes[0], redNotes, redNoteStyle);
+      staveNotes[0] = styleKeys(staveNotes[0], greenNotes, greenNoteStyle);
+
+      // Create a voice in 4/4 and add the notes from above
+      const voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
+      voice.addTickables(staveNotes);
+      // Format and justify the notes to 400 pixels.
+      const formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
+
+      // Render voice
+      voice.draw(context, stave);
+
+      // const beams = VF.Beam.generateBeams(notes);
+      Vex.Flow.Formatter.FormatAndDraw(context, stave, staveNotes);
+      // beams.forEach((b) => { b.setContext(context).draw(); });
+      return (() => {
+        context.closeGroup();
+        context.svg.removeChild(group);
+      });
+    }
+  }, [clef, greenNotes, notes, redNotes, renderer]);
   return (
     <div ref={elemRef} />
   );
