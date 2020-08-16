5adf57ecbb89d4b0c3954020b5d3d77f21049e2b
Prem Netsuwan <grnnja@gmail.com>
2020-08-16 13:16:20 -0700

    remove extraneous comments

--- src/Pages/Level/components/OnScreenKeyboard.jsx
+++ src/Pages/Level/components/OnScreenKeyboard.jsx
@@ -1,14 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 import 'react-piano/dist/styles.css';
 import './customPianoStyles.css'; // import a set of overrides
 import { useSelector } from 'react-redux';
 
-// import playNote from '../../../lib/playSound/playSound';
-
 const OnScreenKeyboard = () => {
   const instrument = useSelector((state) => state.soundPlayer.instrument);
-  console.log('instrument', instrument);
   const firstNote = MidiNumbers.fromNote('c3');
   const lastNote = MidiNumbers.fromNote('f5');
   const keyboardShortcuts = KeyboardShortcuts.create({
@@ -17,18 +14,11 @@ const OnScreenKeyboard = () => {
     keyboardConfig: KeyboardShortcuts.HOME_ROW,
   });
 
-  // const { loading, playNote } = useSound();
-
-  // console.log(soundPlayerLoading);
-  // console.log("loading", loading)
-
   return (
     <Piano
       disabled={instrument === undefined}
       noteRange={{ first: firstNote, last: lastNote }}
       playNote={(midiNumber) => {
-        // instrument.play(midiNumber);
-        // playSound(midiNumber);
         instrument.play(midiNumber);
       }}
       stopNote={() => {}}
--- src/lib/soundPlayer/soundPlayer.js
+++ src/lib/soundPlayer/soundPlayer.js
@@ -1,19 +1,5 @@
 import Soundfont from 'soundfont-player';
 import { setLoading, setInstrument } from './actions';
-// import { store } from '../../App/App';
-
-// const SoundPlayer = () => {
-// const noteBuffer = useSelector((state) => state.noteBuffer);
-// const [loading, setLoading] = useState(true);
-// const [instrument, setInstrument] = useState();
-
-// useEffect(() => {
-
-// let instrument;
-// export let loading = true;
-
-// let instrument;
-// async function initialize() {
 
 const startSoundPlayer = (store) => {
   const ac = new AudioContext();
@@ -24,30 +10,3 @@ const startSoundPlayer = (store) => {
 };
 
 export default startSoundPlayer;
-// .then((piano) => {
-//   console.log('piano loaded')
-//   // instrument = piano;
-//   store.dispatch(setInstrument(piano));
-//   store.dispatch(setLoading(false));
-//   // loading = false;
-// });
-// // }
-
-// export const isLoading = () => {
-//   return loading;
-// };
-
-// function playNote(note) {
-//   // if (_loading) {
-//   // instrument = await initialize();
-//   // loading = false;
-//   // }
-//   // await initialize().then(() => {
-
-//   // });
-//   // console.log(instrument);
-//   instrument.play(note);
-//   // }
-// }
-
-// export default playNote;
